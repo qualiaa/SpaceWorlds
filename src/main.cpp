2def84cae4d064bfe50541b0eb78245f3218df43
Gazok <jamiebayne@hotmail.com>
2014-08-25 11:59:23 +0100

    Window caption added

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 int main()
 {
     tank::Game::initialize({800,600});
+    tank::Game::window()->setCaption("Worlds somehow not in space?.jpg");
     tank::Game::makeWorld<Universe>();
     tank::Game::run();
 }
