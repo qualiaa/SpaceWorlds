56b5754964c931e24dcac2fb20a5840785eb7067
David Truby <david.truby@gmail.com>
2014-08-25 15:45:48 +0100

    Fixed planet wierdness.

--- src/Dialog.cpp
+++ src/Dialog.cpp
@@ -1,12 +1,15 @@
 #include "Dialog.hpp"
 
 tank::Font Dialog::font;
+bool Dialog::font_loaded = false;
 
 Dialog::Dialog(tank::Vectorf pos, std::string text) : Entity(pos)
 {
-    font.loadFromFile("assets/fonts/PlanetFont.ttf");
-    text_ = makeGraphic<tank::Text>(font);
-    text_->setText(text);
+    if (!font_loaded) {
+        font.loadFromFile("assets/fonts/PlanetFont.ttf");
+        font_loaded = true;
+    }
+    text_ = makeGraphic<tank::Text>(font, 30, text);
     text_->setColor(tank::Color::White);
     text_->setScale(1.0/3);
 }
